b6aff4dde5574147c18193ec4f345111689d635b
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-06-11 14:25:58 +0200

    change date stamp in message card to use date-fns library

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { formatDistance } from "date-fns"
 import MessageCardButton from "./MessageCardButton"
 import LikeButton from "./LikeButton"
 import Tag from "./Tag"
@@ -11,31 +12,6 @@ const MessageCard = ({ message, onLike, onDelete, onEdit }) => {
   const [showInput, setShowInput] = useState(false)
   const maxCharacters = 140
   const minCharacters = 5
-
-  // function to format timestamp
-  const toElapsedTime = (timeStamp) => {
-    const unixTimeStamp = Date.parse(timeStamp)
-    const now = Date.now()
-    const timeDiff = now - unixTimeStamp
-
-    const seconds = Math.floor(timeDiff / 1000)
-    if (seconds < 60) return `${seconds} second${seconds !== 1 ? "s" : ""} ago`
-
-    const minutes = Math.floor(seconds / 60)
-    if (minutes < 60) return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`
-
-    const hours = Math.floor(minutes / 60)
-    if (hours < 24) return `${hours} hour${hours !== 1 ? "s" : ""} ago`
-
-    const days = Math.floor(hours / 24)
-    if (days < 30) return `${days} day${days !== 1 ? "s" : ""} ago`
-
-    const months = Math.floor(days / 30)
-    if (months < 12) return `${months} month${months !== 1 ? "s" : ""} ago`
-
-    const years = Math.floor(days / 365)
-    return `${years} year${years !== 1 ? 's' : ''} ago`
-  }
 
   const handleTyping = (event) => {
     setEditedMessage(event.target.value)
@@ -125,7 +101,7 @@ const MessageCard = ({ message, onLike, onDelete, onEdit }) => {
           <p
             className="text-[#707070] text-sm"
           >
-            {toElapsedTime(message.createdAt)}
+            {formatDistance(message.createdAt, new Date(), { addSuffix: true })}
           </p>
         </div>
       </div>
